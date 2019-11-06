0c4e88ab947bf25ec8d6cd116b11a8d0bc347f61
cyuamber <xuranyjy@chinamobile.com>
2019-11-06 19:09:07 +0800

    feat: change locale for antd

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -25,14 +25,6 @@ class Appheader extends React.Component {
         this.setState({
             currentLanguage: currentLanguage === 'ch' ? 'en' : 'ch'
         })
-        let orignalSetItem = window.localStorage.setItem;
-        window.localStorage.setItem = function (key, newValue) {
-            let setItemEvent = new Event("setItemEvent");
-            setItemEvent.key = key;
-            setItemEvent.newValue = newValue;
-            window.dispatchEvent(setItemEvent);
-            orignalSetItem.apply(this, arguments);
-        };
 
         window.localStorage.setItem("currentLng", currentLanguage);
         this.props.i18n.changeLanguage(currentLanguage);
