4546bf4fe448180a6921d62b38f4f5967c90f887
cyuamber <xuranyjy@chinamobile.com>
2019-11-11 09:59:29 +0800

    Finish the layout of the create order page

--- src/constant/constants.js
+++ src/constant/constants.js
@@ -5,41 +5,46 @@ export const ORDER_CREATE_FORM = [{
     {
         title: '连接规模数',
         key: 'connectCount',
-        options: ['10000以下', '10000-50000', '50000-100000', '100000以上']
+        content: '取值范围： 1-100000'
     },
     {
-        title: '宽带',
-        key: 'broadBand',
-        options: ['5G', '10G', '50G']
+        title: '用户下行带宽（Mbps）',
+        key: 'downstream',
+        content: '取值范围： 100-3000'
     },
     {
         title: '时延',
         key: 'delay',
-        options: ['20ms', '50ms', '100ms']
+        content: '取值范围： 10-200'
     },
     {
-        title: '游牧性',
-        key: 'nomadic',
-        options: ['stationary', 'nomadic', 'restricted mobility', 'fully mobility']
-    },
-    {
-        title: '区域',
-        key: 'area'
-    },
-    {
-        title: '使用期限',
-        key: 'timeLimit',
-        options: ['粒度6个月', '粒度12个月', '粒度24个月', '粒度36个月', '自定义月']
+        title: '用户上行带宽（Mbps）',
+        key: 'upstream',
+        content: '取值范围： 100-3000'
     },
     {
         title: '共享等级',
         key: 'shareLevel'
     },
     {
-        title: '服务保障参数',
-        key: 'serviceKey',
-        options: ['钻石套餐', '黄金套餐', '白银套餐']
+        title: '游牧性',
+        key: 'nomadic',
+        options: ['不可游牧', '固定接入', '限定范围内可游牧', '自由游牧']
     },
+    {
+        title: '激活因子（%）',
+        key: 'factor',
+        content: '取值范围： 1-100'
+    },
+    {
+        title: '使用期限（月）',
+        key: 'timeLimit',
+        content: '取值范围： ≥1'
+    },
+    {
+        title: '区域',
+        key: 'area'
+    }
 ]
 
 export const SELECT_OPTIONS = [
--- src/pages/BusinessOrder/BusinessOrderDetail.js
+++ src/pages/BusinessOrder/BusinessOrderDetail.js
@@ -3,7 +3,7 @@ import { withNamespaces } from 'react-i18next'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
 import { actions } from './actions'
-import { Card, Form, Col, Input, Select, Radio, Button, Row, } from "antd";
+import { Card, Form, Col, Input, Select, Radio, Button, Row, Popover, Icon } from "antd";
 import { ORDER_CREATE_FORM } from '../../constant/constants'
 import { axiospost } from '../../utils/http'
 import APIS from '../../constant/apis'
@@ -11,9 +11,27 @@ import APIS from '../../constant/apis'
 import './BusinessOrderDetail.less';
 
 class BusinessOrderDetail extends Component {
-    state = {}
+    state = {
+        formItem: ['area0'],
+    }
 
     getRules = (message, initialValue) => ({ rules: [{ required: true, message }], initialValue })
+
+    handleAddArea = () => {
+        let formItem = [...this.state.formItem]
+        if(formItem.length === 10){
+            return
+        }
+        formItem.push('area' + formItem.length)
+        this.setState({formItem})
+    }
+
+    handleReduceArea = (index) => {
+        let formItem = [...this.state.formItem]
+        formItem.splice(index,1)
+        this.setState({formItem})
+    }
+
 
     getFormItem = () => {
         const { form: { getFieldDecorator } } = this.props;
@@ -22,21 +40,10 @@ class BusinessOrderDetail extends Component {
         const countyList = this.props.businessorder.get('countyList').toJS()
         const { Item } = Form
         const { Option } = Select
+        const { formItem } = this.state
 
         return ORDER_CREATE_FORM.map(item => {
-            if (item.options) {
-                return (
-                    <Col span={12} key={item.key}>
-                        <Item label={item.title}>
-                            {getFieldDecorator(item.key, this.getRules(`${item.title} is required`, item.options[0]))(
-                                <Select>
-                                    {item.options.map(item => <Select.Option key={item}>{item}</Select.Option>)}
-                                </Select>
-                            )}
-                        </Item>
-                    </Col>
-                )
-            } else if (item.key === 'businessName') {
+            if (item.key === 'businessName') {
                 return (
                     <Col span={12} key={item.key}>
                         <Item label={item.title}>
@@ -44,39 +51,31 @@ class BusinessOrderDetail extends Component {
                         </Item>
                     </Col>
                 )
-            } else if (item.key === 'area') {
+            } else if (item.content) {
                 return (
-                    <Col key={item.key} span={12}>
-                        <Col span={12}>
-                            <Item label={item.title} labelCol={{ span: 10, offset: 2 }} wrapperCol={{ span: 10, offset: 2 }}>
-                                {getFieldDecorator('province', this.getRules('Please select a region'))(
-                                    <Select placeholder="省" onChange={this.changeProvince}>
-                                        {provinceList.map(item => <Option key={item.id} value={item.province}>{item.province}</Option>)}
-                                    </Select>
-                                )}
-                            </Item>
-                        </Col>
-                        <Col span={6}>
-                            <Item wrapperCol={{ span: 20, offset: 2 }}>
-                                {getFieldDecorator('city', this.getRules('Please select a region'))(
-                                    <Select placeholder="市" onChange={this.changeCity}>
-                                        {cityList.map(item => <Option key={item.id} value={item.city}>{item.city}</Option>)}
-                                    </Select>
-                                )}
-                            </Item>
-                        </Col>
-                        <Col span={6}>
-                            <Item wrapperCol={{ span: 20, offset: 0 }}>
-                                {getFieldDecorator('county', this.getRules('Please select a region'))(
-                                    <Select placeholder="区" >
-                                        {countyList.map(item => <Option key={item.id} value={item.county}>{item.county}</Option>)}
-                                    </Select>
-                                )}
-                            </Item>
-                        </Col>
+                    <Col span={12} key={item.key}>
+                        <Item label={item.title}>
+                            <Popover placement="right" content={item.content} trigger="click">
+                                {getFieldDecorator(item.key, this.getRules(`${item.title} is required`))(<Input />)}
+                            </Popover>
+                        </Item>
                     </Col>
                 )
-            } else {
+            } else if (item.options) {
+                return (
+                    <Col span={12} key={item.key}>
+                            
+                            <Item label={item.title}>
+                                {getFieldDecorator(item.key, this.getRules(`${item.title} is required`, item.options[0]))(
+                                    <Select>
+                                        {item.options.map(item => <Select.Option key={item}>{item}</Select.Option>)}
+                                    </Select>
+                                
+                                )}
+                        </Item>
+                    </Col>
+                )
+            } else if (item.key === 'shareLevel') {
                 return (
                     <Col key={item.key} span={12}>
                         <Item label={item.title}>
@@ -88,6 +87,72 @@ class BusinessOrderDetail extends Component {
                             )}
                         </Item>
                     </Col>
+                )
+            } else if (item.key === 'area') {
+                return (
+                    formItem.map((it, index) => {
+                        let formItemLayout =null
+                        if(it === formItem[0]){
+                            formItemLayout = {
+                                label: item.title,
+                                labelCol: {span: 10, offset: 0},
+                                wrapperCol: {span: 11, offset: 2}
+                            }
+                        }else{
+                            formItemLayout = {
+                                wrapperCol: {span: 11, offset: 12}
+                            }
+                        }
+                        return (<Col span={24} key={it}>
+                            <Col span={8}>
+                                <Item {...formItemLayout}>
+                                    {getFieldDecorator('province'+it, this.getRules('Please select a region'))(
+                                        <Select placeholder="省" onChange={this.changeProvince}>
+                                            {provinceList.map(item => <Option key={item.id} value={item.province}>{item.province}</Option>)}
+                                        </Select>
+                                    )}
+                                </Item>
+                            </Col>
+                            <Col span={4}>
+                                <Item wrapperCol={{ span: 22, offset: 0 }}>
+                                    {getFieldDecorator('city'+it, this.getRules('Please select a region'))(
+                                        <Select placeholder="市" onChange={this.changeCity}>
+                                            {cityList.map(item => <Option key={item.id} value={item.city}>{item.city}</Option>)}
+                                        </Select>
+                                    )}
+                                </Item>
+                            </Col>
+                            <Col span={4}>
+                                <Item wrapperCol={{ span: 22, offset: 0 }}>
+                                    {getFieldDecorator('county'+it, this.getRules('Please select a region'))(
+                                        <Select placeholder="区" >
+                                            {countyList.map(item => <Option key={item.id} value={item.county}>{item.county}</Option>)}
+                                        </Select>
+                                    )}
+                                </Item>
+                            </Col>
+                            <Col span={4}>
+                                <Item wrapperCol={{ span: 22, offset: 0 }}>
+                                    {getFieldDecorator('street'+it, this.getRules('Please select a region'))(
+                                        <Select placeholder="街道" >
+                                            {countyList.map(item => <Option key={item.id} value={item.county}>{item.county}</Option>)}
+                                        </Select>
+                                    )}
+                                </Item>
+                            </Col>
+                        {it === formItem[0] ? (<Icon 
+                                                    type="plus-square" 
+                                                    theme="filled" 
+                                                    className={formItem.length === 10 ? 'orderdetail_icon_disabled':"orderdetail_icon"}
+                                                    onClick={this.handleAddArea}
+                                                />) : (<Icon
+                                                        type="minus-square" 
+                                                        theme="filled"
+                                                        className="orderdetail_icon" 
+                                                        onClick={() => this.handleReduceArea(index)}
+                                                    />)}
+                    </Col>)
+                    })
                 )
             }
         })
@@ -143,11 +208,11 @@ class BusinessOrderDetail extends Component {
 
     render() {
         const { t } = this.props
-        const formItemLayout = { labelCol: { span: 5, offset: 1 }, wrapperCol: { span: 8, offset: 1 } }
+        const formItemLayout = { labelCol: { span: 8, offset: 0 }, wrapperCol: { span: 8, offset: 0 }}
         return (
             <div className="orderdetail">
                 <Card title={t('Create Slicing Order')}>
-                    <Form {...formItemLayout}>
+                    <Form {...formItemLayout} labelAlign='left'>
                         <Row className="orderdetail_formItem__margin">
                             {this.getFormItem()}
                         </Row>
