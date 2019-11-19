14b7aa71384452fe36b50e9666b76ff2286b4719
cyuamber <xuranyjy@chinamobile.com>
2019-11-19 15:13:09 +0800

    feat: Modify the interface data structure and fields of the order management page

--- src/pages/OrderManagement/actions.js
+++ src/pages/OrderManagement/actions.js
@@ -12,13 +12,13 @@ export const actions = dispatch => {
             
             // APIS.getOrdersApi(reqBody)
             axiosget(APIS.getOrders).then(res => {
-                let { result_body, result_header: { result_code } } = res
+                let { result_body: {record_number, slicing_order_list}, result_header: { result_code } } = res
                 if (result_code === '200') {
-                    let tableData = result_body.map((item, index) => {
+                    let tableData = slicing_order_list.map((item, index) => {
                         item.index = (pageNo - 1) * pageSize + index + 1
                         return item
                     })
-                    dispatch({ type: 'GET_TABLE_DATA', tableData, bool: false, total: res.total, pageSize, pageNo })
+                    dispatch({ type: 'GET_TABLE_DATA', tableData, bool: false, total: record_number*1, pageSize, pageNo })
                 }
             })
         }
