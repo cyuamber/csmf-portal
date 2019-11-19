1739966f9c76a8d3e7c1192e81bce623515dc756
cyuamber <xuranyjy@chinamobile.com>
2019-11-19 16:42:32 +0800

    feat: The creation time in the order management page is formatted

--- src/pages/OrderManagement/actions.js
+++ src/pages/OrderManagement/actions.js
@@ -1,3 +1,4 @@
+import moment from 'moment'
 import { axiosget } from '../../utils/http'
 import APIS from '../../constant/apis'
 
@@ -16,6 +17,7 @@ export const actions = dispatch => {
                 if (result_code === '200') {
                     let tableData = slicing_order_list.map((item, index) => {
                         item.index = (pageNo - 1) * pageSize + index + 1
+                        item.order_creation_time = moment(item.order_creation_time*1).format('YYYY-MM-DD')
                         return item
                     })
                     dispatch({ type: 'GET_TABLE_DATA', tableData, bool: false, total: record_number*1, pageSize, pageNo })
